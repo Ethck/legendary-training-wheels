8d2d6c92e54dbbe328b03d98873832b15d608a5e
Ethck <ethck0720@gmail.com>
2020-10-21 11:51:00 -0700

    Temp combatant working properly.

--- wheels.js
+++ wheels.js
@@ -1,23 +1,25 @@
 import { TemporaryCombatantForm } from "../combat-utility-belt/modules/temporary-combatants/form.js";
 
 Hooks.on("createCombatant", async (currCombat, currToken, options, currID) => {
-    await new Promise(r => setTimeout(r, 20));
+    await new Promise(r => setTimeout(r, 200));
 
-    if (currToken.actor.data.data.resources.lair.value){
-        if (!currCombat.getFlag("legendary-training-wheels", "lair")){
-            await currCombat.setFlag("legendary-training-wheels", "lair", {value: 20})
-            console.log("Flag set.");
+    // Does this actor have lair actions?
+    if (hasProperty(currToken, "actor.data.data.resources.lair.value") && currToken.actor.data.data.resources.lair.value){
+        // Have we already made a Lair Action Combatant?
+        if(!currCombat.combatants.find((combatant) => combatant.token.name === "Lair Action")){
+            const temporaryCombatantForm = new TemporaryCombatantForm({});
+            // Render form, wait to fill
+            await temporaryCombatantForm.render(true);
+            await new Promise(r => setTimeout(r, 20));
+            // fill
+            let tcForm = $(temporaryCombatantForm.form);
+            tcForm.find('[name="name"]').val("Lair Action");
+            tcForm.find('[name="init"]').val(20);
+            tcForm.find('[name="hidden"]').attr("checked", true);
+            // submit / create temp combatant
+            temporaryCombatantForm.submit();
         }
-
-        let acts = currToken.actor.data.data.resources.legact.value;
-        
-        const temporaryCombatantForm = new TemporaryCombatantForm({});
-        await temporaryCombatantForm.render(true);
-        await new Promise(r => setTimeout(r, 20));
-        let tcForm = $(temporaryCombatantForm.form);
-        tcForm.find('[name="name"]').val("Lair Action");
-        tcForm.find('[name="init"]').val(20);
-        tcForm.find('[name="hidden"]').attr("checked", true);
-        temporaryCombatantForm.submit();
     }
-})
+});
+
+
